7bbf34cb6209083cea0e1581ee85574863190597
GeorgePaps <georgezpaps@outlook.com.gr>
2026-06-21 22:20:41 +0300

    Minor updates

--- FlowField/sketch.js
+++ FlowField/sketch.js
@@ -1,15 +1,26 @@
 let width = 1200;
 let height = 800;
 
-const P = palettes.Monochrome; // Select the first palette for now
-
-let field = [];
+let grid = [];
 
 function setup() {
   createCanvas(width, height);
+  resolution = floor(min(width, height) / 200);
+
+  xMin = - width / 2;
+  xMax = width * 1.5;
+  yMin = -height / 2;
+  yMax = height * 1.5;
+
+  // Create a grid of angles
+  // for (let i = 0; i < width; i += resolution) {
+  //   for (let j = 0; j < height; j += resolution) {
+    
+  //   }
+  // }
+
 }
 
 function draw() {
-  background(P.background);
-  console.log(P.background);
+  background(245);
 }
