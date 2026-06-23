d894aea851b2aaab0b71d3a1003f066d0d7d338a
GeorgePaps <georgezpaps@outlook.com.gr>
2026-06-23 08:45:26 +0300

    First flow field implementation

--- FlowField/sketch.js
+++ FlowField/sketch.js
@@ -1,26 +1,68 @@
-let width = 1200;
-let height = 800;
+const width = 1200;
+const height = 800;
+
+const leftBorder = -width / 2;
+const rightBorder = width * 1.5;
+const topBorder = -height / 2;
+const bottomBorder = height * 1.5;
+
+const resolution = Math.floor(Math.min(width, height) / 20);
+
+const numCols = Math.floor((rightBorder - leftBorder) / resolution);
+const numRows = Math.floor((bottomBorder - topBorder) / resolution);
 
 let grid = [];
 
 function setup() {
+
+  // Create Background Canvas
   createCanvas(width, height);
-  resolution = floor(min(width, height) / 200);
 
-  xMin = - width / 2;
-  xMax = width * 1.5;
-  yMin = -height / 2;
-  yMax = height * 1.5;
+  // Setup
+  angleMode(DEGREES);
 
-  // Create a grid of angles
-  // for (let i = 0; i < width; i += resolution) {
-  //   for (let j = 0; j < height; j += resolution) {
-    
-  //   }
-  // }
-
+  // Create Grid
+  for (let row = 0; row < numRows; row++) {
+    grid[row] = [];
+    for (let col = 0; col < numCols; col++) {
+      grid[row][col] = createCell(row, col);
+    }
+  }
 }
 
+  
 function draw() {
   background(245);
+
+    for (let row = 0; row < numRows; row++) {
+      for (let col = 0; col < numCols; col++) {
+        drawArrow(grid[row][col]);
+    }
+  }
 }
+
+
+function drawArrow(cell) {
+
+  push();
+  translate(cell.x, cell.y);
+  rotate(cell.angle);
+  stroke(0);
+  // Draw Arrow
+  line(0, 0, resolution, 0);
+  line(resolution, 0, resolution - 5, -5);
+  line(resolution, 0, resolution - 5, 5);
+  pop();
+  
+} // end drawArrow function
+
+function createCell(row, col) {
+  return { 
+    row: row,
+    col: col,
+    x: leftBorder + col * resolution,
+    y: topBorder + row * resolution,
+    angle: 80
+  };
+} // end createCell function
+  
