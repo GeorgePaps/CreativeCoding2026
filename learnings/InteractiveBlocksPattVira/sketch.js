0155b7aa9f7a62ecc69e0f3bb772e862a37a282c
GeorgePaps <georgezpaps@outlook.com.gr>
2026-06-24 08:49:08 +0300

    PattVira tutorial

--- learnings/InteractiveBlocksPattVira/sketch.js
+++ learnings/InteractiveBlocksPattVira/sketch.js
@@ -1,34 +1,41 @@
+/*
+----- Coding Tutorial by Patt Vira ----- 
+Name: Interactive Display with Rotating Blocks 
+Video Tutorial: https://youtu.be/Fratkco5ACw?si=e9dHp6tCyNA1DrdY
 
+Connect with Patt: @pattvira
+https://www.pattvira.com/
+----------------------------------------
+*/
 
-let distMouse = 50;
-let cols; let rows; let size = 50;
+let distMouse = 15;
+let cols; let rows; let size = 10; let offset = 4;
 let blocks = [];
 
 function setup() {
   createCanvas(400, 400);
   rectMode(CENTER);
   angleMode(DEGREES);
-  cols = width / size;
-  rows = height / size;
-
-  for (let i = 0; i < cols; i++) {
-    blocks[i] = []; 
-    for (let j = 0; j < rows; j++) {
-      blocks[i][j] = new block(i * size , j * size);
+  cols = width/size;
+  rows = height/size;
+  
+  for (let i=0; i<cols; i++) {
+    blocks[i] = [];
+    for (let j=0; j<rows; j++) {
+      blocks[i][j] = new Block(size/2 + i*size, size/2 + j*size);
     }
   }
 
-  b = new block(width / 2, height / 2);
 }
 
 function draw() {
-
-  background(220);
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
+  background(0);
+  for (let i=0; i<cols; i++) {
+    for (let j=0; j<rows; j++) {
       blocks[i][j].move();
       blocks[i][j].display();
     }
+  } 
+
   
-  }
 }
